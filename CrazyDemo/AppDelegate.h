f6215956440f6409cb6db43f2cead36399e09df3
Huang <huangxikai@gmail.com>
2018-09-14 16:43:30 +0800

    Initial Commit

---
+++ CrazyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrazyDemo
+//
+//  Created by Dazzle on 2018/9/14.
+//  Copyright © 2018年 Dazzle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
